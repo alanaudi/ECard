7ff17c10542328d6fd1fc69325173079e5a701e3
alanaudi <alanaudi.tw@gmail.com>
2020-05-16 18:28:46 +0800

    (feat): Init the players

---
+++ color.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# {{{
+# Standard import
+
+# Third-party import
+
+# Local import
+
+# }}}
+
+
+class Color:
+    """ ANSI Color Code """
+    WHITE = '\033[1;37m'
+    BLACK = '\033[0;30m'
+    RED = '\033[0;31m'
+    YELLOW = '\033[1;33m'
+    GRAY = '\033[0;37m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+
+    HEADER = '\033[95m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
--- ecard.py
+++ ecard.py
@@ -1,8 +1,5 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
-# Copyright © 2020 alanaudi <alanaudi.tw@gmail.com>
-#
 
 # {{{
 # Standard import
@@ -15,6 +12,7 @@ from functools import reduce
 
 # Local import
 from player import Player
+from color import Color
 # }}}
 
 def typing(msg):
@@ -23,26 +21,56 @@ def typing(msg):
         sys.stdout.write(char)
         sys.stdout.flush()
 
-def show_cards(cards):
-    hand = reduce(lambda x, y: [x[i] + y[i] for i in range(6)], [card.skin for card in cards])
-    print('\n'.join(hand))
-
 class ECard:
     def __init__(self, line:int=5):
         self.slave = Player("伊藤開司", "Slave")
         self.emperor = Player("利根川幸雄", "Emperor")
+        self.winner = None
+        self.loser = None
+        self.players = []
 
-    def play(self):
+
+    @staticmethod
+    def show_cards(cards):
+        hand = reduce(lambda x, y: [x[i]+y[i] for i in range(6)], [card.skin for card in cards])
+        print('\n'.join(hand))
+
+    def start(self, name):
+        self.slave.name = name
+
         # Initialize Game Message
-        typing(F'Welcome to the ECard Game, {self.slave.name}\n')
+        typing(F'Welcome to the ECard Game, {Color.BLUE}{self.slave.name}{Color.ENDC}\n')
         typing(F'I\'m {self.emperor.name}\n')
         sleep(0.5)
         typing(F'In this game, you have the following 5 cards,\n')
-        show_cards(self.slave.hand.cards)
+        self.show_cards(self.slave.hand.cards)
+        sleep(1)
+        typing(F'And I also have 5 cards below.\n')
+        self.show_cards(self.emperor.hand.cards)
+        typing(F'Emperor counter citizen, citizen counter slave and slave counter emperor.\n')
+
+    def who_first(self, side):
+        self.slave.side = 'offensive' if side == 'Slave' else 'defensive'
+        self.emperor.side = 'offensive' if side == 'Emperor' else 'defensive'
+        self.players.append(self.slave if self.slave.side == 'offensive' else self.emperor)
+        self.players.append(self.slave if self.slave.side == 'defensive' else self.emperor)
+        typing(F'Now the offensive side is {(self.players[0]).name} and the defensive side is {(self.players[1]).name}\n')
+
+    def act(self):
+        print([(player.name, player.side) for player in self.players])
+
+
 
 def main():
+    name = input('What\'s your name, challenger.\n')
     game = ECard(line=5)
-    game.play()
+    game.start(name)
+    first = input('Please select the role who first.\n❯ Slave\n❯ Emperor\n')
+    game.who_first(first)
+    # while game.winner is not None:
+    game.act()
+    # while game.winner is None:
+
 
 
 if "__main__" == __name__:
